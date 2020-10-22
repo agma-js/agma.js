56f6cf02190f641cc44292ab987dc8ae16ec37bc
Ivan Petrukhin <uWynell@yandex.ru>
2020-10-22 21:16:33 +0300

    0.4.0

--- src/features/getServers.ts
+++ src/features/getServers.ts
@@ -4,7 +4,6 @@ import { Server } from '../entities/Server';
 
 /**
  * Get all available servers or all available server with specified location
- * @param location The server location
  */
 export const getServers = async (): Promise<Server[]> => {
   const { data } = await axios.get('https://agma.io/php_hscores_file.php', { params: { type: 3 } });
