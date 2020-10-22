a8374f6a798a4813f610a5a3f9e84f5b6d377192
Ivan Petrukhin <uWynell@yandex.ru>
2020-10-22 21:15:28 +0300

    Remove unnecessary tests and features

--- src/entities/Server.ts
+++ src/entities/Server.ts
@@ -1,22 +1,12 @@
 /**
- * Agma.IO available locations
- */
-export type Location = 'eu' | 'na' | 'as';
-
-/**
  * Server object from API response
  */
 export type ServerData = {
   serverId: string;
   serverName: string;
-  serverLocation: '0' | '1' | '2';
-  active: '0' | '1';
+  serverLocation: string;
+  active: string;
 };
-
-/**
- * Agma.IO locations list
- */
-export const locations: Location[] = ['eu', 'na', 'as'];
 
 /**
  * Check if Agma.IO API response valid
@@ -24,14 +14,7 @@ export const locations: Location[] = ['eu', 'na', 'as'];
  */
 // eslint-disable-next-line
 export const isServerData = (data: any): data is ServerData =>
-  'serverId' in data &&
-  !isNaN(data['serverId']) &&
-  'serverName' in data &&
-  data['serverName'].length > 0 &&
-  'serverLocation' in data &&
-  ['0', '1', '2'].includes(data['serverLocation']) &&
-  'active' in data &&
-  ['0', '1'].includes(data['active']);
+  'serverId' in data && 'serverName' in data && 'serverLocation' in data && 'active' in data;
 
 /**
  * The server entity
@@ -48,7 +31,7 @@ export class Server {
   /**
    * The server location
    */
-  location: Location = locations[0];
+  location = 0;
   /**
    * Is server online?
    */
@@ -60,9 +43,9 @@ export class Server {
   static fromData(data: unknown): Server {
     const server = new Server();
     if (!isServerData(data)) throw new TypeError('The given data is not assignable to type ServerData');
-    server.id = parseInt(data.serverId);
+    server.id = Number(data.serverId);
     server.name = data.serverName;
-    server.location = locations[parseInt(data.serverLocation)];
+    server.location = Number(data.serverLocation);
     server.online = Boolean(Number(data.active));
     return server;
   }
--- src/entities/__tests__/Server.test.ts
+++ src/entities/__tests__/Server.test.ts
@@ -2,24 +2,16 @@ import { Server } from '../Server';
 
 const idealServerData = { serverId: '40', serverName: 'Infection EU (Beta)', serverLocation: '0', active: '1' };
 const incompleteServerData = { serverId: '40', serverName: 'Infection EU (Beta)' };
-const invalidServerId = { serverId: 'foo', serverName: 'Infection EU (Beta)', serverLocation: '0', active: '1' };
-const invalidServerName = { serverId: '40', serverName: '', serverLocation: '0', active: '1' };
-const invalidServerLocation = { serverId: '40', serverName: 'Infection EU (Beta)', serverLocation: '3', active: '1' };
-const invalidServerActive = { serverId: '40', serverName: 'Infection EU (Beta)', serverLocation: '0', active: '2' };
 
 it('works in case of a good server data', () => {
   expect(Server.fromData(idealServerData)).toMatchObject({
     id: 40,
-    location: 'eu',
+    location: 0,
     name: 'Infection EU (Beta)',
     online: true,
   });
 });
 
-it('throws a error in case of an invalid server data', () => {
-  expect(() => Server.fromData(invalidServerId)).toThrow('The given data is not assignable to type ServerData');
-  expect(() => Server.fromData(invalidServerName)).toThrow('The given data is not assignable to type ServerData');
-  expect(() => Server.fromData(invalidServerLocation)).toThrow('The given data is not assignable to type ServerData');
-  expect(() => Server.fromData(invalidServerActive)).toThrow('The given data is not assignable to type ServerData');
+it('throws a error in case of incomplete server data', () => {
   expect(() => Server.fromData(incompleteServerData)).toThrow('The given data is not assignable to type ServerData');
 });
